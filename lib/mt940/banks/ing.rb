681a0251fac7754e079f1c9f6955c89fadd94ef6
Lars Vonk <lars.vonk@gmail.com>
2014-02-14 13:22:59 +0100

    Make sure new MT940 formats from ING, introduced by IBAN, are also correctly parsed.

--- lib/mt940/banks/ing.rb
+++ lib/mt940/banks/ing.rb
@@ -1,10 +1,17 @@
 class MT940::Ing < MT940::Base
 
+  IBAN_BIC_R = /^([a-zA-Z]{2}[0-9]{2}[a-zA-Z0-9]{0,30})(?:\s)([a-zA-Z0-9]{8,11})(?:\s)(.*)/
+  CONTRA_ACCOUNT_DESCRIPTION_R = /^(.*)(?:\s)(?:NOTPROVIDED)(?:\s)(.*)/
+
+
   def self.determine_bank(*args)
     self if args[0].match(/INGBNL/)
   end
 
   def parse_tag_61
+
+    @is_structured_format = @line.match /EREF|PREF|MARF|\d{16}/
+
     if @line.match(/^:61:(\d{6})(C|D)(\d+),(\d{0,2})N(\S+)/)
       sign = $2 == 'D' ? -1 : 1
       @transaction = MT940::Transaction.new(:bank_account => @bank_account, :amount => sign * ($3 + '.' + $4).to_f, :bank => @bank, :currency => @currency)
@@ -18,14 +25,10 @@ class MT940::Ing < MT940::Base
   end
 
   def parse_contra_account
-    if @transaction && @transaction.description.match(/([P|\d]\d{9})?(.+)/)
-      @transaction.description = $2
-      number = $1
-      unless number.nil?
-        @transaction.contra_account = number.gsub(/\D/, '').gsub(/^0+/, '')
-      else
-        @transaction.contra_account = "NONREF"
-      end
+    if @is_structured_format && @transaction && @transaction.description.match(IBAN_BIC_R)
+      parse_structured_description $1, $3
+    elsif @transaction && @transaction.description.match(/([P|\d]\d{9})?(.+)/)
+      parse_description $1, $2
     end
   end
 
@@ -51,4 +54,26 @@ class MT940::Ing < MT940::Base
   ING_MAPPING["Code"]= "Toelichting"
   ING_MAPPING["CHK"]= "Cheque"
   ING_MAPPING["TRF"]= "Overboeking buitenland"
+
+  private
+  # introduced by IBAN
+  def parse_structured_description(iban, description)
+    @transaction.contra_account_iban=iban
+    @transaction.description=description
+    @transaction.contra_account=@transaction.contra_account_iban[8..-1].sub(/^0+/, '') if @transaction.contra_account_iban.match /^NL/
+    if @transaction.description.match CONTRA_ACCOUNT_DESCRIPTION_R
+      @transaction.contra_account_owner=$1
+      @transaction.description=$2
+    end
+  end
+
+  def parse_description(account_number, description)
+    @transaction.description = description.strip
+    number = account_number
+    unless number.nil?
+      @transaction.contra_account = number.gsub(/\D/, '').gsub(/^0+/, '')
+    else
+      @transaction.contra_account = "NONREF"
+    end
+  end
 end
--- spec/mt940_ing_spec.rb
+++ spec/mt940_ing_spec.rb
@@ -2,61 +2,226 @@ require_relative 'spec_helper'
 
 describe "ING" do
 
-  before :each do
-    @file_name = File.dirname(__FILE__) + '/fixtures/ing.txt'
-    @bank_statements = MT940::Base.parse_mt940(@file_name)["1234567"]
-    @transactions = @bank_statements.flat_map(&:transactions)
-    @transaction = @transactions.first
+  context 'old mt940' do
+    before :each do
+      @file_name = File.dirname(__FILE__) + '/fixtures/ing.txt'
+      @bank_statements = MT940::Base.parse_mt940(@file_name)["1234567"]
+      @transactions = @bank_statements.flat_map(&:transactions)
+      @transaction = @transactions.first
+    end
+
+    it 'have the correct number of transactions' do
+      @transactions.size.should == 6
+    end
+
+    it 'get the opening balance and date' do
+      @bank_statements.first.previous_balance.amount.should == 0
+      @bank_statements.first.previous_balance.date.should == Date.new(2010, 7, 22)
+    end
+
+    it 'get the closing balance and date' do
+      @bank_statements.last.new_balance.amount.should == 3.47
+      @bank_statements.last.new_balance.date.should == Date.new(2010, 7, 23)
+    end
+
+    context 'Transaction' do
+
+      it 'have a bank_account' do
+        @transaction.bank_account.should == '1234567'
+      end
+
+      it 'have an amount' do
+        @transaction.amount.should == -25.03
+      end
+
+      it 'have a currency' do
+        @transaction.currency.should == 'EUR'
+      end
+
+      it 'have a date' do
+        @transaction.date.should == Date.new(2010, 7, 22)
+      end
+
+      it 'return its bank' do
+        @transaction.bank.should == 'Ing'
+      end
+
+      it "should return the type" do
+        @transaction.type.should == 'Overschrijving'
+      end
+
+      it 'have a description' do
+        @transactions.last.description.should == 'EJ46GREENP100610T1456 CLIEOP TMG GPHONGKONG AMSTERDAM'
+      end
+
+      it 'return the contra_account' do
+        @transactions.last.contra_account.should == 'NONREF'
+      end
+
+    end
+
   end
-  
-  it 'have the correct number of transactions' do
-    @transactions.size.should == 6
+
+  context 'new mt940' do
+
+    before :each do
+      @file_name = File.dirname(__FILE__) + '/fixtures/ing_structured.txt'
+      @bank_statements = MT940::Base.parse_mt940(@file_name)["1234567"]
+      @transactions = @bank_statements.flat_map(&:transactions)
+      @transaction = @transactions.first
+    end
+
+    it 'has the correct number of transactions' do
+      @transactions.size.should == 7
+    end
+
+    it 'get the opening balance and date' do
+      @bank_statements.first.previous_balance.amount.should == 500
+      @bank_statements.first.previous_balance.date.should == Date.new(2013, 6, 30)
+    end
+
+    it 'get the closing balance and date' do
+      @bank_statements.last.new_balance.amount.should == 400
+      @bank_statements.last.new_balance.date.should == Date.new(2013, 10, 1)
+    end
+
+    context 'Transaction' do
+      before :each do
+        @transaction = @transactions[1]
+      end
+
+      it 'have a bank_account' do
+        @transaction.bank_account.should == '1234567'
+      end
+
+      it 'have an amount' do
+        @transaction.amount.should == 300
+      end
+
+      it 'have a currency' do
+        @transaction.currency.should == 'EUR'
+      end
+
+      it 'have a date' do
+        @transaction.date.should == Date.new(2013, 7, 1)
+      end
+
+      it 'return its bank' do
+        @transaction.bank.should == 'Ing'
+      end
+
+      it "should return the type" do
+        @transaction.type.should == 'Overschrijving'
+      end
+
+      it 'have a description' do
+        @transaction.description.should == 'VAN Zkl Kwartaal Spaarrekening'
+      end
+
+      it 'return the contra_account' do
+        @transaction.contra_account.should == '1234567'
+      end
+
+    end
+
+    context 'IBAN transaction' do
+      before :each do
+        @transaction = @transactions[2]
+      end
+
+      it 'have a bank_account' do
+        @transaction.bank_account.should == '1234567'
+      end
+
+      it 'have an amount' do
+        @transaction.amount.should == -400
+      end
+
+      it 'have a currency' do
+        @transaction.currency.should == 'EUR'
+      end
+
+      it 'have a date' do
+        @transaction.date.should == Date.new(2013, 7, 1)
+      end
+
+      it 'return its bank' do
+        @transaction.bank.should == 'Ing'
+      end
+
+      it "should return the type" do
+        @transaction.type.should == 'Internetbankieren'
+      end
+
+      it 'have a description' do
+        @transaction.description.should == 'kilometervergoed ing 2e kwartaal 2013'
+      end
+
+      it 'returns the contra_account' do
+        @transaction.contra_account.should == '987654321'
+        @transaction.contra_account_iban.should == 'NL57ABNA0987654321'
+        @transaction.contra_account_owner.should == 'J AAP'
+      end
+
+    end
+
+    context 'IBAN transaction from tax' do
+      before :each do
+        @transaction = @transactions[4]
+      end
+
+      it 'have a description' do
+        @transaction.description.should == 'BELASTINGDIENST BTW 2e kwartaal 2013 SCOR/CUR/8850426741301240'
+      end
+
+      it 'has an account and iban' do
+        @transaction.contra_account.should == '2445588'
+        @transaction.contra_account_iban.should == 'NL86INGB0002445588'
+      end
+
+      it 'is unable to parse the contra account owner' do
+
+        @transaction.contra_account_owner.should be_nil
+      end
+    end
+
+    context 'IBAN transaction with EREF NOTPROVIDED' do
+      before :each do
+        @transaction = @transactions[3]
+      end
+
+      it 'have a description' do
+        @transaction.description.should == 'parkeren'
+      end
+
+      it 'has an account and iban' do
+        @transaction.contra_account.should == '987654321'
+        @transaction.contra_account_iban.should == 'NL57ABNA0987654321'
+      end
+
+      it 'has a contra account owner' do
+        @transaction.contra_account_owner.should == 'J AAP / FOO B.V.'
+      end
+
+    end
+
+    context 'IBAN transaction with EREF' do
+      before :each do
+        @transaction = @transactions.last
+      end
+
+      it 'have a description' do
+        @transaction.description.should == 'J AAP kilometervergoeding 3e kwart aal'
+      end
+
+      it 'has an account and iban' do
+        @transaction.contra_account.should == '987654321'
+        @transaction.contra_account_iban.should == 'NL57ABNA0987654321'
+      end
+
+      it 'is unable to parse the contra account owner' do
+        @transaction.contra_account_owner.should be_nil
+      end
+    end
   end
-
-  it 'get the opening balance and date' do
-    @bank_statements.first.previous_balance.amount.should == 0
-    @bank_statements.first.previous_balance.date.should == Date.new(2010, 7, 22)
-  end
-
-  it 'get the closing balance and date' do
-    @bank_statements.last.new_balance.amount.should == 3.47
-    @bank_statements.last.new_balance.date.should == Date.new(2010, 7, 23)
-  end
-
-  context 'Transaction' do
-
-    it 'have a bank_account' do
-      @transaction.bank_account.should == '1234567'
-    end
-
-    it 'have an amount' do
-      @transaction.amount.should == -25.03
-    end
-
-    it 'have a currency' do
-      @transaction.currency.should == 'EUR'
-    end
-
-    it 'have a date' do
-      @transaction.date.should == Date.new(2010,7,22)
-    end
-
-    it 'return its bank' do
-      @transaction.bank.should == 'Ing'
-    end
-
-    it "should return the type" do
-      @transaction.type.should == 'Overschrijving'
-    end
-
-    it 'have a description' do
-      @transactions.last.description.should == 'EJ46GREENP100610T1456 CLIEOP TMG GPHONGKONG AMSTERDAM'
-    end
-
-    it 'return the contra_account' do
-      @transactions.last.contra_account.should == 'NONREF'
-    end
-
-  end
-
 end
