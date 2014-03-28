36c8691e9e72cb49e3c0b7e76b6ce52e6ef08a3e
Lars Vonk <lars.vonk@gmail.com>
2014-03-28 21:24:55 +0100

    Fixed parsing multiple accounts in single rabobank file.

--- lib/mt940_structured/parser.rb
+++ lib/mt940_structured/parser.rb
@@ -1,10 +1,13 @@
 module MT940Structured
   class Parser
     def self.parse_mt940(path)
-      file_content = FileContent.new(File.open(path).readlines.map { |line| line.encode('UTF-8', 'binary', :invalid => :replace, :undef => :replace) })
+      file_content = FileContent.new(File.open(path).readlines.map do |line|
+        line
+        .gsub(/\u001A/, '') # remove eof chars in the middle of the string... yes it happens :-(
+        .encode('UTF-8', 'binary', :invalid => :replace, :undef => :replace) # remove other obscure chars. god knows what people upload.
+      end)
       grouped_lines = file_content.group_lines
       file_content.get_header.parser.transform(grouped_lines)
     end
-
   end
 end
