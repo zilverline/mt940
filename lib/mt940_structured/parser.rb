4b4da315f820de4b1663ce7e81b4dc479dc28807
Gareth Stenson <gareth.stenson@redhills.ie>
2014-09-25 22:25:53 +0100

    fixing temp file load

--- lib/mt940_structured/parser.rb
+++ lib/mt940_structured/parser.rb
@@ -7,15 +7,15 @@ module MT940Structured
       file_content.get_header.parser.transform(grouped_lines)
     end
 
-    def self.parse_mt940_stream(stream, join_lines_by = ' ')
-      file_content = FileContent.new(readfile(stream), join_lines_by)
+    def self.parse_mt940_temp_file(stream, join_lines_by = ' ')
+      file_content = FileContent.new(readfile(stream.tempfile.open), join_lines_by)
       grouped_lines = file_content.group_lines
       file_content.get_header.parser.transform(grouped_lines)
     end
 
     private
-    def self.readfile(streamio)
-      streamio.readlines.map do |line|
+    def self.readfile(stringio)
+      stringio.readlines.map do |line|
         line
           .encode('UTF-8', 'binary', :invalid => :replace, :undef => :replace) # remove other obscure chars. god knows what people upload.
           .gsub(/\u001A/, '') # remove eof chars in the middle of the string... yes it happens :-(
