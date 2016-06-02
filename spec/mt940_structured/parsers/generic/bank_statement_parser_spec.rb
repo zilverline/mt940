9fa52c49172cd8bf1d4bb22047c086708d468b20
Gareth Stenson <gareth.stenson@cashanalytics.com>
2016-06-02 16:40:05 +0100

    fix for optional field

--- spec/mt940_structured/parsers/generic/bank_statement_parser_spec.rb
+++ spec/mt940_structured/parsers/generic/bank_statement_parser_spec.rb
@@ -12,7 +12,7 @@ describe MT940Structured::Parsers::BankStatementParser do
           ":28C:160/1",
           ":60F:C130402EUR000000001147,95",
           ":61:130403D000000000127,50N102NONREF TEST SAMPLE",
-          ":61:091211DR000000000002,50N001NONREF",
+          ":61:091211CR000000000002,50N001NONREF",
           ":86:/EREF/02-04-2013 22:56 1120000153447185/BENM//NAME/Nespresso Nede rland B.V./REMI/674725433 1120000153447185 14144467636004962 /ISDT/2013-04-03",
           ":62F:C130404EUR000000018846,34"
       ]
@@ -31,7 +31,7 @@ describe MT940Structured::Parsers::BankStatementParser do
     	expect(subject.transactions.last.bank_reference).to eq('')
     end
     it 'has the correct type when book date not present' do
-      expect(subject.transactions.last.type).to eq('DR')
+      expect(subject.transactions.last.type).to eq('CR')
     end
     it 'has the should have a regular date' do
       expect(subject.transactions.last.date).to_not be_nil
