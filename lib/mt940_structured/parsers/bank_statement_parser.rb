328f3a5eb93c649f9c4c106b72159db974115f97
Chris Blake <chris.blake@redhills.ie>
2017-07-26 09:58:30 +0100

    parse_line_25(line) issue

--- lib/mt940_structured/parsers/bank_statement_parser.rb
+++ lib/mt940_structured/parsers/bank_statement_parser.rb
@@ -35,9 +35,11 @@ module MT940Structured::Parsers
         @bank_statement.bank_account = $1.gsub(/^0+/, '')
         @is_structured_format = true
       when /^:\d{2}:\D*(\d*)/
-        # Rolled back to previous as the account number was getting truncated when it had a - 
+        # Rolled back to previous as the account number ($1) was getting truncated when it had a - 
+        # Also added the removal of leading 0's to the original
         # @bank_statement.bank_account = $1.gsub(/\D/, '').gsub(/^0+/, '')
-        @bank_statement.bank_account = line[4 .. -1]
+        # @bank_statement.bank_account = line[4 .. -1] #original
+        @bank_statement.bank_account = line[4 .. -1].gsub(/^0+/, '')
         @is_structured_format = false
        when /^:\d{2}:IE/
           #puts "B4"
