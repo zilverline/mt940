fe7587c65ac07e0f8e14495fbb43de4bb78c4752
Chris Blake <chris.blake@redhills.ie>
2017-08-03 12:18:18 +0100

    nedbank parser

--- lib/mt940_structured/parsers/nedbank/transaction_parser.rb
+++ lib/mt940_structured/parsers/nedbank/transaction_parser.rb
@@ -6,7 +6,8 @@ module MT940Structured::Parsers::Nedbank
     include MT940Structured::Parsers::DefaultLine61Parser
 
     def parse_transaction(line_61)
-      if line_61.match(/^:61:(\d{6})(?:\d{4})?(C|D)\s(\d+),(\d{0,2})N(.{3})([a-zA-Z\d\s?]{1,16})(\-?.{1,16})?/)
+      # if line_61.match(/^:61:(\d{6})(?:\d{4})?(C|D)\s(\d+),(\d{0,2})N(.{3})([^\s?]{1,16})(\-?.{1,16})?/)
+      if line_61.match(/^:61:(\d{6})(?:\d{4})?(C|D)\s(\d+),(\d{0,2})N(.{3})(.*$)?/)
         sign = $2 == 'D' ? -1 : 1
         transaction = MT940::Transaction.new(:amount => sign * ($3 + '.' + $4).to_f)
         transaction.type = $2
